c3d2ad79b286bc804bc39f82d712fb5247ed691f
Abdul Abbasi <asabbasi2006.2@gmail.com>
2025-12-08 18:14:36 +0000

    bug fixes

--- src/member/advertise.rs
+++ src/member/advertise.rs
@@ -1,9 +1,11 @@
-use std::{collections::{BTreeMap, BTreeSet}, fs, str::FromStr, time::Duration};
+use std::{collections::{BTreeMap, BTreeSet}, fs, time::Duration};
 use bluer::adv::Advertisement;
-use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce, aead::{Aead, OsRng, rand_core::RngCore}};
+use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce, aead::{Aead, OsRng, rand_core::RngCore, AeadCore}};
 use base64::{Engine, engine::general_purpose};
-use tokio::{io::BufReader, time::sleep, };
-use uuid::Uuid;
+use tokio::{io::{BufReader, AsyncBufReadExt}, time::sleep};
+use uuid::{uuid, Uuid};
+
+type NonceSize = AeadCore::NonceSize;
 
 #[tokio::main]
 pub(crate) async fn main() -> bluer::Result<()> {
@@ -25,19 +27,19 @@ pub(crate) async fn main() -> bluer::Result<()> {
 
     let mut nonce_bytes = [0u8; 12];
     OsRng.fill_bytes(&mut nonce_bytes);
-    let nonce = Nonce::from_slice(&nonce_bytes);
+    let nonce: &Nonce<NonceSize> = Nonce::from_slice(&nonce_bytes);
 
     let asset_id = fs::read_to_string("asset-tag")?.trim().to_string();
     let asset_id_hash = blake3::hash(&asset_id.as_bytes());
 
-    let ciphertext = cipher.encrypt(nonce, asset_id_hash.as_bytes())?;
+    let ciphertext = cipher.encrypt(nonce, asset_id_hash.as_bytes()[..]).unwrap();
 
     let mut payload = Vec::new();
     payload.push(0xA1);
     payload.extend_from_slice(&nonce_bytes);    
     payload.extend_from_slice(&ciphertext);
 
-    const SERVICE_UUID: Uuid = Uuid::from_str("c193b907-6f78-4769-aafa-83e807c9c0a6").unwrap();
+    const SERVICE_UUID: Uuid = uuid!("c193b907-6f78-4769-aafa-83e807c9c0a6");
 
     let mut service_uuids = BTreeSet::new();
     service_uuids.insert(SERVICE_UUID);
