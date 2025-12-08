63e520ff7c4206b62e74c678f9b5f8182ba18ff5
Abdul Abbasi <asabbasi2006.2@gmail.com>
2025-12-08 16:31:17 +0000

    basic advertisement

---
+++ src/member/advertise.rs
@@ -0,0 +1,71 @@
+use std::{collections::{BTreeMap, BTreeSet}, fs, str::FromStr, time::Duration};
+use bluer::adv::Advertisement;
+use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce, aead::{Aead, OsRng, rand_core::RngCore}};
+use base64::{Engine, engine::general_purpose};
+use tokio::{io::BufReader, time::sleep, };
+use uuid::Uuid;
+
+#[tokio::main]
+pub(crate) async fn main() -> bluer::Result<()> {
+    env_logger::init();
+
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+    adapter.set_powered(true).await?;
+
+    println!("Advertising on Bluetooth adapter {} with address {}", adapter.name(), adapter.address().await?);
+
+    let key: [u8; 32] = general_purpose::STANDARD
+        .decode(fs::read_to_string("group-key")?.trim())
+        .expect("invalid key file")
+        .try_into()
+        .expect("invalid key");
+    let key = Key::from_slice(&key);
+    let cipher = Aes256Gcm::new(key);
+
+    let mut nonce_bytes = [0u8; 12];
+    OsRng.fill_bytes(&mut nonce_bytes);
+    let nonce = Nonce::from_slice(&nonce_bytes);
+
+    let asset_id = fs::read_to_string("asset-tag")?.trim().to_string();
+    let asset_id_hash = blake3::hash(&asset_id.as_bytes());
+
+    let ciphertext = cipher.encrypt(nonce, asset_id_hash.as_bytes())?;
+
+    let mut payload = Vec::new();
+    payload.push(0xA1);
+    payload.extend_from_slice(&nonce_bytes);    
+    payload.extend_from_slice(&ciphertext);
+
+    const SERVICE_UUID: Uuid = Uuid::from_str("c193b907-6f78-4769-aafa-83e807c9c0a6").unwrap();
+
+    let mut service_uuids = BTreeSet::new();
+    service_uuids.insert(SERVICE_UUID);
+
+    let mut service_data = BTreeMap::new();
+    service_data.insert(SERVICE_UUID, payload);
+
+    let advertisement = Advertisement {
+        advertisement_type: bluer::adv::Type::Peripheral,
+        service_uuids,
+        service_data,
+        discoverable: Some(true),
+        ..Default::default()
+    };
+
+    println!("{:?}", &advertisement);
+
+    let handle = adapter.advertise(advertisement).await?;
+
+    println!("Press enter to quit");
+
+    let stdin = BufReader::new(tokio::io::stdin());
+    let mut lines = stdin.lines();
+    let _ = lines.next_line().await;
+
+    println!("Removing advertisement");
+    drop(handle);
+    sleep(Duration::from_secs(1)).await;
+
+    Ok(())
+}
---
+++ src/member/main.rs
@@ -0,0 +1,5 @@
+mod advertise;
+
+fn main() {
+    advertise::main();
+}
